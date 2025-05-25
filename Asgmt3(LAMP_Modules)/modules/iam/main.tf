b065353318966e4f5998d70ae078f24143a6e823
sync19850320 <kimurashinya19850320@gmail.com>
2025-05-25 20:03:34 +0900

    Asgmt3 iam json外部化

--- Asgmt3(LAMP_Modules)/modules/iam/main.tf
+++ Asgmt3(LAMP_Modules)/modules/iam/main.tf
@@ -10,13 +10,13 @@ resource "aws_iam_role" "ec2_role" {
 }
 
 resource "aws_iam_policy" "ec2_policy" {
-    name        = "${var.environment}-${var.project_name}-ec2-policy"
+    name        = "${var.environment}-${var.project_name}-ec2-policy-v1"
     description = "Policy for EC2 instance"
-    policy      = var.policy_document
+    policy      = file("${path.module}/policies/ec2_policy.json")
 }
 
 resource "aws_iam_role_policy_attachment" "ec2_role_policy" {
-    policy_arn = aws_iam_policy.ec2.policy.arn
+    policy_arn = aws_iam_policy.ec2_policy.arn
     role = aws_iam_role.ec2_role.name
 }
 
